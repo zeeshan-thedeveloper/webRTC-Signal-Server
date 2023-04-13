53ab57fb12a2ed81e83561256eaedbf58879beb3
Zeeshan <zeeshanahmedd0010@gmail.com>
2023-04-13 22:43:29 +0500

    create base

--- index.js
+++ index.js
@@ -16,175 +16,81 @@ const server = app.listen(8080, () => {
 const io = require("socket.io")(server);
 
 io.on("connection", (socket) => {
+
   console.log("New user joined socket : " + socket.id);
-  io.to(socket.id).emit("response", "You are connected to socket");
-  io.to(socket.id).emit("socketId", socket.id);
-
-  socket.on("save-icecandidate", ({ socketId, iceCandidates }, callback) => {
-    console.log(`Received new ice-candidate object request from ${socketId}`);
-    // Store the join offer in the cache for the given call ID and socket ID
-    localStorageManager.storeICECandidate(socketId, iceCandidates);
-    console.log(
-      `Stored iceCandidates object for socket ${socketId} : `,
-      localStorageManager.getICECandidate(socketId)
-    );
-    const status = {
+  io.to(socket.id).emit("recieveSocketId", socket.id);
+  
+  socket.on("startOneToOneCall",({socketId}, callback) => {
+    
+    const response = {
       success: true,
-      message: "ICE-Candidate stored successfully.",
-    };
-    callback(status);
-  });
-
-  socket.on("getCallId", (data) => {
-    console.log(`Received getCallId request from ${socket.id}`);
-
-    // Generate a new call ID (you can use a UUID library here)
-    const callId = Math.floor(Math.random() * 1000000).toString();
-
-    // Send the call ID back to the socket that made the request
-    socket.emit("sentCallId", { callId });
-
-    console.log(`Sent callId ${callId} to ${socket.id}`);
-  });
-
-  socket.on(
-    "initiate-call",
-    ({ userName, callId, offer, socketId }, callback) => {
-      console.log(
-        `Received call initiation request from ${userName} for call ${callId}.`
-      );
-
-      localStorageManager.storeOffer(callId, socketId, offer);
-      console.log(
-        `Stored offer for call ${callId} : `,
-        localStorageManager.getOffer(callId)
-      );
-      const callStatus = {
-        success: true,
-        message: `Call initiated for ${callId}.`,
-      };
-      callback(callStatus);
-    }
-  );
-
-  socket.on("makeJoinRequest", ({ userName, callId, socketId }, callback) => {
-    console.log(
-      `Received join call request from ${userName} for call ${callId}.`
-    );
-
-    // Retrieve the socket ID associated with the call ID from the cache
-    // this socket id is the id of one who has created meeting or call
-    const hostSocketId = localStorageManager.getCallCreatorSocketId(callId);
-    console.log(`socket id of host of call ${callId} is : `, hostSocketId);
-    if (!hostSocketId) {
-      const error = {
-        success: false,
-        message: `Socket ID not found for call ${callId}.`,
-      };
-      socket.emit("makeJoinRequestError", error);
-      return;
-    }
-
-    let hostIceCandidates = localStorageManager.getICECandidate(hostSocketId);
-
-    // Store the join offer in the cache for the given call ID and socket ID
-    localStorageManager.storeJoinRequest(callId, socketId, userName);
-
-    console.log(
-      "Stored join request : ",
-      localStorageManager.getJoinRequest(socketId)
-    );
-
-    // Emit the "joinRequest" event to the socket ID associated with the call ID
-    const joinData = {
-      userName,
-      socketId,
-      id: new Date().getMilliseconds(),
+      message: "Request startOneToOneCall recieved successfully.",
     };
 
-    io.to(hostSocketId).emit("joinRequest", joinData);
-    // Return success status to the client
-    const status = {
+    callback(response)
+  })
+
+  socket.on("makeOneToOneCallJoiningRequest",({socketId}, callback) => {
+    
+    const response = {
       success: true,
-      hostIceCandidates:hostIceCandidates,
-      message: "Join request sent successfully.",
-    };
-    callback(status);
-  });
-
-  socket.on("sendAcceptJoinRequest", ({ callId, socketId }, callback) => {
-    console.log(`Received accept join request for ${callId} of ${socketId}`);
-
-    // Retrieve the socket ID associated with the call ID from the cache
-    // this socket id is the id of one who has created meeting or call
-    const offer = localStorageManager.getOffer(callId);
-
-    if (!offer) {
-      const error = {
-        success: false,
-        message: `Offer  not found for id ${callId}.`,
-      };
-      socket.emit("sendAcceptJoinRequestError", error);
-      return;
-    }
-
-    // Emit the "requestStatus" event to the socket ID associated with the call ID
-
-    const joinData = {
-      remoteOffer: offer.offer,
-      isAccepted: true,
-      id: new Date().getMilliseconds(),
+      message: "Request makeOneToOneCallJoiningRequest recieved successfully.",
     };
 
-    io.to(socketId).emit("requestStatus", joinData);
+    callback(response)
+  })
 
-    // Return success status to the client
-    const status = {
+  socket.on("updateOneToOneCallRequestStatus",({socketId}, callback) => {
+    
+    const response = {
       success: true,
-      message: "Request acceptance sent successfully.",
+      message: "Request updateOneToOneCallRequestStatus recieved successfully.",
     };
-    callback(status);
-  });
 
-  socket.on(
-    "joinCall",
-    ({ userName, callId, joinOffer, socketId,localIceCandidates }, callback) => {
-      console.log(
-        `Received  join call request for ${callId} from ${socketId} with userName ${userName}`
-      );
+    callback(response)
+  })
 
-      // Retrieve the socket ID associated with the call ID from the cache
-      const hostSocketId = localStorageManager.getCallCreatorSocketId(callId);  
-      if (!hostSocketId) {
-        const error = {
-          success: false,
-          message: `Socket is not found for call id ${callId}.`,
-        };
-        socket.emit("joinCallError", error);
-        return;
-      }
 
-      //storing localIceCandidates
-      localStorageManager.storeICECandidate(socketId,localIceCandidates);
-      // Emit the "requestStatus" event to the socket ID associated with the call ID
-      const data = {
-        remoteOffer: joinOffer,
-        userName,
-        socketId,
-        remoteICECandidates:localIceCandidates,
-        id: new Date().getMilliseconds(),
-      };
+  socket.on("startGroupCall",({socketId}, callback) => {
+    
+    const response = {
+      success: true,
+      message: "Request startGroupCall recieved successfully.",
+    };
 
-      io.to(hostSocketId).emit("userToAddInCall", data);
+    callback(response)
+  })
 
-      // Return success status to the client
-      const status = {
-        success: true,
-        message: "Request acceptance sent successfully.",
-      };
-      callback(status);
-    }
-  );
+
+  socket.on("makeGroupCallJoiningRequest",({socketId}, callback) => {
+    
+    const response = {
+      success: true,
+      message: "Request makeGroupCallJoiningRequest recieved successfully.",
+    };
+
+    callback(response)
+  })
+
+  socket.on("updateGroupCallRequestStatus",({socketId}, callback) => {
+    
+    const response = {
+      success: true,
+      message: "Request updateGroupCallRequestStatus recieved successfully.",
+    };
+
+    callback(response)
+  })
+
+  socket.on("joinGroupCall",({socketId}, callback) => {
+    
+    const response = {
+      success: true,
+      message: "Request joinGroupCall recieved successfully.",
+    };
+
+    callback(response)
+  })
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
--- storage-manager/storage-manager.js
+++ storage-manager/storage-manager.js
@@ -1,96 +1,131 @@
+
 const storageManager = () => {
-  const offerCache = new Map();
-  const joinOfferCache = new Map();
-  const joinRequestCache = new Map();
-  const iceCandidatesCache = new Map();
-  // Store an offer in the cache for a given call ID and socket ID
-  const storeOffer = (callId, socketId, offer) => {
-    offerCache.set(callId, { socketId, offer });
-    console.log(`Stored offer for call ${callId} in cache.`);
+
+  const calls = new Map();
+  const joinRequests = new Map();
+
+  // Calls related operation
+  const addNewCall = (
+    callId,
+    callType,
+    hostName,
+    hostId,
+    hostSocketId,
+    callTitle,
+    callDescription
+  ) => {
+    let call = {
+      callType,
+      hostId,
+      hostSocketId,
+      hostName,
+      callDetails: {
+        callTitle,
+        callDescription,
+      },
+      callCandidates: [],
+      hostICECandidates: [],
+    };
+
+    calls.set(callId, call);
+
+    return call;
   };
 
-  // Retrieve an offer from the cache for a given call ID
-  const getOffer = (callId) => {
-    const offer = offerCache.get(callId);
-    if (!offer) {
-      console.log(`No offer found for call ${callId} in cache.`);
-      return null;
+  const addCandidateToCall = (
+    callId,
+    candidateName,
+    candidateSocketId,
+    isConnected
+  ) => {
+    let call = calls.get(callId);
+    let candidate = {
+      candidateName,
+      candidateSocketId,
+      isConnected,
+      iceCandidates: [],
+    };
+    call.callCandidates.push(candidate);
+    calls.set(callId, call);
+    return call;
+  };
+
+  const addHostICECandidatesIntoCall = (callId, hostICECandidates) => {
+    let call = calls.get(callId);
+    call.hostICECandidates = hostICECandidates;
+    calls.set(callId, call);
+    return call;
+  };
+
+  const addIceCandidatesIntoCandidate = (
+    callId,
+    candidateSocketId,
+    iceCandidates
+  ) => {
+    let call = calls.get(callId);
+    call.callCandidates.map((candidate) => {
+      if (candidate.candidateSocketId === candidateSocketId) {
+        candidate.iceCandidates = iceCandidates;
+        return candidate;
+      } else {
+        return candidate;
+      }
+    });
+
+    calls.set(callId, call);
+
+    return call;
+  };
+
+  const updateIsConnectedFlagForCandidate=(callId,candidateSocketId,isConnected)=>{
+    let call = calls.get(callId);
+    call.callCandidates.map((candidate) => {
+      if (candidate.candidateSocketId === candidateSocketId) {
+        candidate.isConnected = isConnected;
+        return candidate;
+      } else {
+        return candidate;
+      }
+    });
+
+    calls.set(callId, call);
+
+    return call;
+  }
+
+  // join requests
+  const addJoinRequest=(callId,requestId,requesterSocketId,requestStatus)=>{
+    let joinRequest={
+      requestId,requesterSocketId,requestStatus
     }
-    console.log(`Retrieved offer for call ${callId} from cache.`);
-    return offer;
-  };
+    let allJoinRequestsOfGivenCallId = joinRequests.get(callId);
+    allJoinRequestsOfGivenCallId.push(joinRequest);
 
-  // Retrieve the socket ID associated with a call ID from the cache
-  const getCallCreatorSocketId = (callId) => {
-    const socketId = offerCache.get(callId);
-    if (!socketId) {
-      console.log(`No socket ID found for call ${callId} in cache.`);
-      return null;
-    }
-    console.log(
-      `Retrieved socket ID ${socketId.socketId} for call ${callId} from cache.`
-    );
-    return socketId.socketId;
-  };
+    joinRequests.set(callId,allJoinRequestsOfGivenCallId);
+    return allJoinRequestsOfGivenCallId;
+  }
 
-  // Store a join offer in the cache for a given call ID and socket ID
-  const storeJoinOffer = (callId, socketId, joinOffer) => {
-    joinOfferCache.set(socketId, { callId, joinOffer });
-    console.log(`Stored join offer for call ${callId} in cache.`);
-  };
-
-  const storeICECandidate = (socketId, iceCandidates) => {
-      iceCandidatesCache.set(socketId, {iceCandidates});
-      console.log(`Stored icecandidate for socketId ${socketId} in cache.`);  
-  };
-
-  const storeJoinRequest = (callId, socketId, userName) => {
-    joinRequestCache.set(socketId, { callId, userName });
-    console.log(`Stored join request for call ${callId} in cache.`);
-  };
-  const getJoinRequest = (socketId) => {
-    const joinRequest = joinRequestCache.get(socketId);
-    if (!joinRequest) {
-      console.log(`No join request found for socket ${socketId} in cache.`);
-      return null;
-    }
-  };
-  const getICECandidate = (socketId) => {
-    const icecandidate = iceCandidatesCache.get(socketId);
-    if (!icecandidate) {
-      console.log(
-        `No ICECandidate object found for socket ${socketId} in cache.`
-      );
-      return null;
-    }
-    console.log(
-      `Retrieved join request for socket ${icecandidate} from cache.`
-    );
-    return icecandidate;
-  };
-  // Retrieve a join offer from the cache for a given socket ID
-  const getJoinOffer = (socketId) => {
-    const joinOfferData = joinOfferCache.get(socketId);
-    if (!joinOfferData) {
-      console.log(`No join offer found for socket ${socketId} in cache.`);
-      return null;
-    }
-    console.log(
-      `Retrieved join offer for call ${joinOfferData.callId} from cache.`
-    );
-    return joinOfferData.joinOffer;
-  };
-
+  const updateJoinRequestStatus=(callId,requestId,requesterSocketId,requestStatus)=>{
+    let allJoinRequestsOfGivenCallId = joinRequests.get(callId);
+    allJoinRequestsOfGivenCallId.map((request)=>{
+      if(request.requestId===requestId){
+        request.requestStatus=requestStatus
+        return request;
+      }
+      return request;
+    })
+    joinRequests.set(callId,allJoinRequestsOfGivenCallId);
+    return allJoinRequestsOfGivenCallId;
+  }
+  
   return {
-    storeOffer,
-    getOffer,
-    getCallCreatorSocketId,
-    storeJoinOffer,
-    getJoinOffer,
-    storeJoinRequest,
-    getJoinRequest,
-    storeICECandidate,
-    getICECandidate
+    addNewCall,
+    addCandidateToCall,
+    addHostICECandidatesIntoCall,
+    addIceCandidatesIntoCandidate,
+    updateIsConnectedFlagForCandidate,
+    addJoinRequest,
+    updateJoinRequestStatus
   };
 };
 
